0b4079fef9c54d2ae7c6566f08d8bb33a9536c87
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-11 00:17:28 +0200

    Update FreeCamera.cs

--- Assets/Movement/FreeCamera.cs
+++ Assets/Movement/FreeCamera.cs
@@ -4,14 +4,31 @@ using UnityEngine;
 
 public class FreeCamera : MonoBehaviour
 {
+    [SerializeField] private float _Speed;
+    [SerializeField] private float _SprintSpeed;
+
+    private float _CurrentSpeed;
 
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftShift))
+            _CurrentSpeed = _SprintSpeed;
+        else
+            _CurrentSpeed = _Speed;
+
+        float xas = Input.GetAxis("Horizontal");
+        float zas = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(xas,0, zas) * _CurrentSpeed * Time.deltaTime);
+
+        float mousex = Input.GetAxis("Mouse X");
+        float mousey = Input.GetAxis("Mouse Y");
+        transform.eulerAngles += new Vector3(-mousey, mousex, 0);
     }
 }
