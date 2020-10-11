1ee74f3dd9e79c71de95316d90ea8d2126ebfd00
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-12 01:45:29 +0200

    Added RayCast_3D.cs

---
+++ Assets/Scripts/RandomExamples/RayCast_3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast_3D : MonoBehaviour
+{
+    [SerializeField] private LayerMask _LayerMask;
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, _LayerMask))
+        {
+            //Hit
+        }
+        else
+        {
+            //No hit
+        }
+    }
+}
