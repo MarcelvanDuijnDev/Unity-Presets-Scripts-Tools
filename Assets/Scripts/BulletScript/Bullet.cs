2216bf82a9e3c66e73c7b4b09f29f8c001d7eec5
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-12 01:39:27 +0200

    Added Bullet.cs

---
+++ Assets/Scripts/BulletScript/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _Speed;
+    [SerializeField] private float _Damage;
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * _Speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "ExampleTag")
+        {
+            //DoDamage
+            gameObject.SetActive(false);
+        }
+    }
+}
