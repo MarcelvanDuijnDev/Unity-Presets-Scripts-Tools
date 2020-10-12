e0d9e87ae8aac2de0927faa2cde14e79e10fe331
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-12 23:22:13 +0200

    Added ScriptebleGameObject.cs

---
+++ Assets/Scripts/RandomExamples/ScriptebleGameObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Example", menuName = "SO/ExampleSO", order = 1)]
+public class ScriptebleGameObject : ScriptableObject
+{
+    public string examplestring;
+    public int exampleint;
+}
