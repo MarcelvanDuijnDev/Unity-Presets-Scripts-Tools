2530db2cb72a9b89844f5578bc62bd6e65a372a7
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-13 17:13:01 +0200

    Added EditorWindowExample.cs

---
+++ Assets/Editor/EditorWindowExample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+class EditorWindowExample : EditorWindow
+{
+    string examplestring = "example";
+    bool examplebool = false;
+
+    [MenuItem("Tools/EditorWindowExample")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(EditorWindowExample));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Example Title", EditorStyles.boldLabel);
+        examplestring = EditorGUILayout.TextField("Example string field", examplestring);
+        examplebool = EditorGUILayout.Toggle("Example bool field", examplebool);
+    }
+}
