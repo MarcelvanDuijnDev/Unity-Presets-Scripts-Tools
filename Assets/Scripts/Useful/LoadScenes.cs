640958c96a1cb70e785b497a5855d0841efd63d2
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-21 22:53:10 +0200

    Added LoadScenes.cs

---
+++ Assets/Scripts/Useful/LoadScenes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void Action_LoadScene(int sceneid)
+    {
+        SceneManager.LoadScene(sceneid);
+    }
+    public void Action_LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void Action_ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Action_QuitApplication()
+    {
+        Application.Quit();
+    }
+}
