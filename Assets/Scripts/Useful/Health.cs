82542a7e79999dc2b377928130b9c82d56794975
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-28 00:36:51 +0100

    Added Health.cs

---
+++ Assets/Scripts/Useful/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _MaxHealth;
+
+    private float _CurrentHealth;
+
+    private void OnEnable()
+    {
+        _CurrentHealth = _MaxHealth;
+    }
+
+    public void DoDamage(float damageamount)
+    {
+        _CurrentHealth -= damageamount;
+        if(_CurrentHealth < 0)
+        {
+            _CurrentHealth = 0;
+            gameObject.SetActive(false);
+        }
+    }
+
+    public float GetCurrentHealth()
+    {
+        return _CurrentHealth;
+    }
+    public float GetMaxHealth()
+    {
+        return GetMaxHealth();
+    }
+}
