8946b19369b7a1079339364c759199c3af1b74d5
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-10-28 01:12:28 +0100

    Added Turret.cs

---
+++ Assets/Scripts/Useful/Turret.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private Vector2 _MinMaxRange;
+    [SerializeField] private float _SecondsBetweenShots;
+    [SerializeField] private float _Damage;
+    [SerializeField] private GameObject _ShootPart;
+    [SerializeField] private string _Tag;
+    
+    private float _Timer;
+    private GameObject _Target;
+
+    void Update()
+    {
+        if (_Target != null)
+        {
+            _ShootPart.transform.LookAt(_Target.transform.position);
+            _Timer += 1 * Time.deltaTime;
+            if (_Timer >= _SecondsBetweenShots)
+            {
+                _Target.GetComponent<Health>().DoDamage(_Damage);
+                _Timer = 0;
+            }
+        }
+        else
+        {
+            _ShootPart.transform.rotation = Quaternion.Euler(90, 0, 0);
+        }
+
+        _Target = FindEnemy();
+    }
+
+    public GameObject FindEnemy()
+    {
+        GameObject[] m_Targets = GameObject.FindGameObjectsWithTag(_Tag);
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+
+        _MinMaxRange.x = _MinMaxRange.x * _MinMaxRange.x;
+        _MinMaxRange.y = _MinMaxRange.y * _MinMaxRange.y;
+        foreach (GameObject target in m_Targets)
+        {
+            Vector3 diff = target.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance && curDistance >= _MinMaxRange.x && curDistance <= _MinMaxRange.y)
+            {
+                closest = target;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+}
