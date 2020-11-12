2ba55e0e5285288630380be04e53e168814e2275
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-11-12 13:57:34 +0100

    Update Tool_ScriptToString.cs

--- Assets/Editor/Tool_ScriptToString.cs
+++ Assets/Editor/Tool_ScriptToString.cs
@@ -36,26 +36,121 @@ public class Tool_ScriptToString : EditorWindow
 
     private string ConvertScriptToString()
     {
-        string scriptasstring = "";
+        _ScriptOutput = "";
+        string scriptasstring = "\"";
 
-        List<string> scriptsplit = _ScriptInput.Split().ToList();
+        //Split / add to array
+        List<string> textedit = new List<string>();
 
-        for (int i = 0; i < scriptsplit.Count; i++)
+
+        for (int i = 0; i < _ScriptInput.Length; i++)
         {
-            if(i + 1 > scriptsplit.Count)
+            textedit.Add(System.Convert.ToString(_ScriptInput[i]));
+        }
+
+        bool headercheck = false;
+        bool commentcheck = false;
+
+        for (int i = 0; i < textedit.Count; i++)
+        {
+            //Header check
+            if (i + 7 < textedit.Count)
             {
-                if(scriptsplit[i+1] == "\"")
+                if (textedit[i] + textedit[i + 1] + textedit[i + 2] + textedit[i + 3] + textedit[i + 4] + textedit[i + 5] + textedit[i + 6] + textedit[i + 7] == "[Header(")
+                    headercheck = true;
+            }
+
+            // Comment check
+            if (i + 1 < textedit.Count)
+            {
+                if (textedit[i] + textedit[i + 1] == "//" || textedit[i] + textedit[i + 1] == "/*")
+                    commentcheck = true;
+            }
+
+
+            //Comment /* + */
+            if (commentcheck)
+            {
+                if (textedit[i - 1] + textedit[i] == "*/")
                 {
-                    scriptsplit.Add("A");
+                    scriptasstring += "\\n";
+                    commentcheck = false;
+                }
+
+                if (i + 6 < textedit.Count)
+                {
+                    //if
+                    if (textedit[i] + textedit[i + 1] == "if")
+                    {
+                        scriptasstring += "\\n";
+                        commentcheck = false;
+                    }
+                    //switch
+                    if (textedit[i] + textedit[i + 1] + textedit[i + 2] + textedit[i + 3] + textedit[i + 4] + textedit[i + 5] == "switch")
+                    {
+                        scriptasstring += "\\n";
+                        commentcheck = false;
+                    }
+                    //case
+                    if (textedit[i] + textedit[i + 1] + textedit[i + 2] + textedit[i + 3] == "case")
+                    {
+                        scriptasstring += "\\n";
+                        commentcheck = false;
+                    }
+                    //public
+                    if (textedit[i] + textedit[i + 1] + textedit[i + 2] + textedit[i + 3] + textedit[i + 4] + textedit[i + 5] == "public")
+                    {
+                        scriptasstring += "\\n";
+                        commentcheck = false;
+                    }
+                    //private
+                    if (textedit[i] + textedit[i + 1] + textedit[i + 2] + textedit[i + 3] + textedit[i + 4] + textedit[i + 5] + textedit[i + 6] == "private")
+                    {
+                        scriptasstring += "\\n";
+                        commentcheck = false;
+                    }
+                    //void
+                    if (textedit[i] + textedit[i + 1] + textedit[i + 2] + textedit[i + 3] == "void")
+                    {
+                        scriptasstring += "\\n";
+                        commentcheck = false;
+                    }
                 }
             }
+
+            scriptasstring += textedit[i];
+
+
+
+            //Endings check
+            if (i + 1 < textedit.Count)
+            {
+                if (textedit[i + 1] == "\"")
+                {
+                    scriptasstring += "\\";
+                }
+
+                if (textedit[i] == "}")
+                {
+                    scriptasstring += "\\n";
+                }
+                if (textedit[i] == ";")
+                {
+                    scriptasstring += "\\n";
+                }
+                if(textedit[i] == "]" && headercheck)
+                {
+                    scriptasstring += "\\n";
+                    headercheck = false;
+                }
+            }
+
+
+
+
         }
 
-        for (int i = 0; i < scriptsplit.Count; i++)
-        {
-            scriptasstring += scriptsplit[i];
-        }
-
+        scriptasstring += "\"";
         return scriptasstring;
     }
 }
