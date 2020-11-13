cb0f406a83d12e34a80a4d60e354b7f5b854aea4
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-11-13 21:09:56 +0100

    Updated Movement_Camera.cs

--- Assets/Scripts/Movement/Movement_Camera.cs
+++ Assets/Scripts/Movement/Movement_Camera.cs
@@ -10,43 +10,77 @@ public class Movement_Camera : MonoBehaviour
     [Header("Options")]
     [SerializeField] private CameraOptionsPos _CameraOptionPos = CameraOptionsPos.Follow;
     [SerializeField] private CameraOptionsRot _CameraOptionRot = CameraOptionsRot.Follow;
-    [Header("Settings Position")]
+    [Header("Settings - Position")]
     [SerializeField] private Vector3 _OffsetPosition = new Vector3(0,12,-4);
     [SerializeField] private bool _UseOffsetYAsDefaultHeight = true;
-    [Header("Settings Rotation")]
+    [Header("Settings - Rotation")]
     [SerializeField] private Vector3 _OffsetRotation;
+    [Header("Settings")]
+    [SerializeField] private float _Speed = 1000;
 
     [Header("Other")]
     [SerializeField] private Transform _Target;
 
+    private Vector3 _TargetPosition;
+    private float _ScreenShakeDuration;
+    private float _ScreenShakeIntensity;
+
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            Effect_ScreenShake(3, 0.5f);
+        }
+
+        //Update Target Location
+        if (_UseOffsetYAsDefaultHeight)
+            _TargetPosition = new Vector3(_Target.transform.position.x + _OffsetPosition.x, _OffsetPosition.y, _Target.transform.position.z + _OffsetPosition.z);
+        else
+            _TargetPosition = new Vector3(_Target.transform.position.x + _OffsetPosition.x, _Target.transform.position.y + _OffsetPosition.y, _Target.transform.position.z + _OffsetPosition.z);
+
         // Movement
-        switch(_CameraOptionPos)
+        switch (_CameraOptionPos)
         {
             case CameraOptionsPos.Follow:
                 if (_UseOffsetYAsDefaultHeight)
-                    transform.position = new Vector3(_Target.position.x + _OffsetPosition.x, _OffsetPosition.y, _Target.position.z + _OffsetPosition.z);
+                    transform.position = Vector3.MoveTowards(transform.position, _TargetPosition, _Speed * Time.deltaTime);
                 else
-                    transform.position = new Vector3(_Target.position.x + _OffsetPosition.x, _Target.position.y + _OffsetPosition.y, _Target.position.z + _OffsetPosition.z);
+                    transform.position = _TargetPosition;
                 break;
         }
-        // Rotation
-        switch(_CameraOptionRot)
+
+        //ScreenShake
+        if(_ScreenShakeDuration > 0)
         {
-            case CameraOptionsRot.Follow:
-                Vector3 rpos = _Target.position - transform.position;
-                Quaternion lookrotation = Quaternion.LookRotation(rpos, Vector3.up);
-                transform.eulerAngles = new Vector3(lookrotation.eulerAngles.x + _OffsetRotation.x, lookrotation.eulerAngles.y + _OffsetRotation.y, lookrotation.eulerAngles.z + _OffsetRotation.z);
-                break;
+            transform.localPosition = new Vector3(_TargetPosition.x + Random.insideUnitSphere.x * _ScreenShakeIntensity, _TargetPosition.y + Random.insideUnitSphere.y * _ScreenShakeIntensity, _TargetPosition.z);
+            _ScreenShakeDuration -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            // Rotation
+            switch (_CameraOptionRot)
+            {
+                case CameraOptionsRot.Follow:
+                    Vector3 rpos = _Target.position - transform.position;
+                    Quaternion lookrotation = Quaternion.LookRotation(rpos, Vector3.up);
+                    transform.eulerAngles = new Vector3(lookrotation.eulerAngles.x + _OffsetRotation.x, lookrotation.eulerAngles.y + _OffsetRotation.y, lookrotation.eulerAngles.z + _OffsetRotation.z);
+                    break;
+            }
         }
     }
 
+    //Effects
+    public void Effect_ScreenShake(float duration, float intesity)
+    {
+        _ScreenShakeDuration = duration;
+        _ScreenShakeIntensity = intesity;
+    }
+
+    //Set
     public void Set_CameraTarget(GameObject targetobj)
     {
         _Target = targetobj.transform;
     }
-
     public void Set_OffSet(Vector3 offset)
     {
         _OffsetPosition = offset;
