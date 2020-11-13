1d0ef08a12e144d1d8b25a8e060772a2fa712be5
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-11-13 02:05:43 +0100

    Added CameraEffects.cs

---
+++ Assets/Scripts/Useful/CameraEffects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraEffects : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Effect_Screenshake()
+    {
+
+    }
+
+    public void Effect_Zoom()
+    {
+
+    }
+}
