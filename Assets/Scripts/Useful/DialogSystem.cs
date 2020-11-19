33cc148a018bb4868774e1507cc6080ba5dad3e0
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-11-19 15:03:20 +0100

    Update DialogSystem.cs

--- Assets/Scripts/Useful/DialogSystem.cs
+++ Assets/Scripts/Useful/DialogSystem.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class DialogSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
