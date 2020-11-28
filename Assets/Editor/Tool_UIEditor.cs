470bfdce98660b11e80250bdae447011c1605711
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2020-11-28 21:16:40 +0100

    Update Tool_UIEditor.cs

--- Assets/Editor/Tool_UIEditor.cs
+++ Assets/Editor/Tool_UIEditor.cs
@@ -4,6 +4,7 @@ using UnityEditor.UI;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 class Tool_UIEditor : EditorWindow
 {
@@ -63,5 +64,54 @@ class Tool_UIEditor : EditorWindow
         Canvas canvascomponent = canvasobj.GetComponent<Canvas>();
         canvascomponent.renderMode = RenderMode.ScreenSpaceCamera;
 
+        GameObject button_start = Create_Button("Button_Start", "Start", new Vector2(40,45.5f), new Vector2(200,30), 10);
+
+
+
+        button_start.transform.parent = canvasobj.transform;
+    }
+
+    GameObject Create_Button(string name, string buttontext, Vector2 pos, Vector2 size, float textoffset)
+    {
+        GameObject buttontemplate = new GameObject();
+        DestroyImmediate(buttontemplate.GetComponent<Transform>());
+        RectTransform buttontransform = buttontemplate.AddComponent<RectTransform>();
+
+        buttontransform.sizeDelta = size;
+
+        buttontransform.anchoredPosition = pos;
+        buttontransform.pivot = new Vector2(0,0);
+        buttontransform.anchorMin = new Vector2(0,0);
+        buttontransform.anchorMax = new Vector2(0,0);
+
+        buttontemplate.AddComponent<CanvasRenderer>();
+        Image buttonimage = buttontemplate.AddComponent<Image>();
+        buttonimage.sprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+        buttonimage.type = Image.Type.Sliced;
+        Button buttonbutton = buttontemplate.AddComponent<Button>();
+        buttonbutton.targetGraphic = buttonimage;
+        buttontemplate.name = name;
+
+        GameObject buttontextemplate = new GameObject();
+        DestroyImmediate(buttontextemplate.GetComponent<Transform>());
+        RectTransform buttontextrect = buttontextemplate.AddComponent<RectTransform>();
+
+        buttontextrect.anchoredPosition = new Vector2(pos.x + textoffset, pos.y);
+        buttontextrect.sizeDelta = size;
+        buttontextrect.pivot = new Vector2(0,0);
+
+        TextMeshProUGUI buttontexttmpro = buttontextemplate.AddComponent<TextMeshProUGUI>();
+        buttontexttmpro.text = buttontext;
+        buttontexttmpro.fontSize = 15;
+        buttontexttmpro.alignment = TextAlignmentOptions.MidlineLeft;
+        buttontexttmpro.color = Color.black;
+
+        buttontextemplate.name = name + "text";
+
+
+
+        buttontextemplate.transform.parent = buttontemplate.transform;
+
+        return buttontemplate;
     }
 }
