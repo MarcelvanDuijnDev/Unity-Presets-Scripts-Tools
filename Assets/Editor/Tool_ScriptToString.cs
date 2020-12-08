9ab9288223e8cd010997c948f69d54e2de280196
Marcel van Duijn <epicmars264@gmail.com>
2020-12-08 01:46:17 +0100

    Update Tool_ScripToString.cs
    Added filter option, custom keywords
    + fixes

--- Assets/Editor/Tool_ScriptToString.cs
+++ Assets/Editor/Tool_ScriptToString.cs
@@ -9,6 +9,11 @@ public class Tool_ScriptToString : EditorWindow
     string _ScriptInput = "";
     string _ScriptOutput = "";
 
+    List<string> _CustomCommandCheckKeywords = new List<string>();
+    string _CustomCommandCheck;
+
+    private Vector2 _ScrollPos = new Vector2();
+
     [MenuItem("Tools/Convert Script to String")]
     public static void ShowWindow()
     {
@@ -17,19 +22,101 @@ public class Tool_ScriptToString : EditorWindow
 
     void OnGUI()
     {
-        //Input
-        GUILayout.Label("Paste script here: ", EditorStyles.boldLabel);
-        _ScriptInput = EditorGUILayout.TextField("", _ScriptInput);
-
-        //Output
-        GUILayout.Label("Converted to string: ", EditorStyles.boldLabel);
-        EditorGUILayout.TextField("", _ScriptOutput);
-
         //Convert
-        if (GUILayout.Button("Convert"))
+        if (GUILayout.Button("Convert", GUILayout.Height(30)))
         {
             _ScriptOutput = ConvertScriptToString();
         }
+
+        GUILayout.Space(20);
+
+        //Input
+        GUILayout.Label("Input: ", EditorStyles.boldLabel);
+        _ScriptInput = EditorGUILayout.TextField("", _ScriptInput);
+
+        //Output
+        GUILayout.Label("Output: ", EditorStyles.boldLabel);
+        EditorGUILayout.TextField("", _ScriptOutput);
+
+        GUILayout.Space(20);
+
+        GUILayout.Label("Use Custom Keywords to fix lines that should not be included into the commend. \n" +
+            "The x on the left shows the lines that contain a comment.");
+
+        GUILayout.Space(20);
+
+        //Other Settings
+        GUILayout.Label("Custom Keywords: ", EditorStyles.boldLabel);
+        _CustomCommandCheck = EditorGUILayout.TextField("", _CustomCommandCheck);
+        if (GUILayout.Button("AddKeyword"))
+        {
+            _CustomCommandCheckKeywords.Add(_CustomCommandCheck);
+            _CustomCommandCheck = "";
+            _ScriptOutput = ConvertScriptToString();
+        }
+        for (int i = 0; i < _CustomCommandCheckKeywords.Count; i++)
+        {
+            GUILayout.BeginHorizontal("box");
+            GUILayout.Label(_CustomCommandCheckKeywords[i]);
+            if (GUILayout.Button("Remove", GUILayout.Width(100)))
+            {
+                _CustomCommandCheckKeywords.RemoveAt(i);
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        //Preview
+        List<string> output = new List<string>();
+        List<string> output2 = new List<string>();
+
+        for (int i = 0; i < _ScriptOutput.Length; i++)
+        {
+            output.Add(System.Convert.ToString(_ScriptOutput[i]));
+        }
+
+        int begincalc = 0;
+        int endcalc = 0;
+
+        for (int i = 0; i < output.Count; i++)
+        {
+            if(i + 1 < output.Count)
+            {
+                if(output[i] + output[i + 1] == "\\n")
+                {
+                    endcalc = i;
+                    string addstring = "";
+                    for (int j = 0; j < endcalc - begincalc; j++)
+                    {
+                        addstring += output[begincalc + j];
+                    }
+                    addstring += output[endcalc] + output[endcalc + 1];
+
+                    output2.Add(addstring);
+                    endcalc = endcalc + 1;
+                    begincalc = endcalc + 1;
+                }
+            }
+        }
+
+        _ScrollPos = EditorGUILayout.BeginScrollView(_ScrollPos);
+
+        for (int i = 0; i < output2.Count; i++)
+        {
+            GUILayout.BeginHorizontal();
+            if(output2[i].Contains("//"))
+            {
+                EditorGUILayout.TextField("", "x", GUILayout.MaxWidth(15));
+            }
+            else
+            {
+                EditorGUILayout.TextField("", "", GUILayout.MaxWidth(15));
+            }
+
+            EditorGUILayout.TextField("", output2[i]);
+            GUILayout.EndHorizontal();
+        }
+        
+        EditorGUILayout.EndScrollView();
     }
 
     private string ConvertScriptToString()
@@ -127,11 +214,24 @@ public class Tool_ScriptToString : EditorWindow
                         scriptasstring += "\\n";
                         commentcheck = false;
                     }
-                    //custom variable
-                    if (textedit[i] == "_")
+                }
+
+                for (int j = 0; j < _CustomCommandCheckKeywords.Count; j++)
+                {
+                    if(_CustomCommandCheckKeywords[j].Length < textedit.Count)
                     {
-                        scriptasstring += "\\n";
-                        commentcheck = false;
+                        string check = "";
+
+                        for (int o = 0; o < _CustomCommandCheckKeywords[j].Length; o++)
+                        {
+                            check += textedit[i + o];
+                        }
+                        
+                        if(check == _CustomCommandCheckKeywords[j])
+                        {
+                            scriptasstring += "\\n";
+                            commentcheck = false;
+                        }
                     }
                 }
             }
