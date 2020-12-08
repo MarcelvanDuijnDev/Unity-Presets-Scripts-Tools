f4862e48ede57d4ecd6a7d81d10d242ab8212d98
Marcel van Duijn <epicmars264@gmail.com>
2020-12-08 21:38:21 +0100

    Added MenuHandler.cs + Update SettingsHandler.cs

---
+++ Assets/Scripts/Useful/MenuHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void LoadScene(int sceneid)
+    {
+        SceneManager.LoadScene(sceneid);
+    }
+
+    public void LoadScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public int Get_CurrentSceneID()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public string Get_CurrentSceneName()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
