82b46e1a8578a49aa7c7fb7ddbbc57994d840962
Marcel van Duijn <epicmars264@gmail.com>
2020-12-21 01:35:32 +0100

    Added PosToPos.cs + Rotation.cs

---
+++ Assets/Scripts/Practical/PosToPos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosToPos : MonoBehaviour
+{
+    [SerializeField] private Transform _GotoPosition = null;
+    [SerializeField] private float _Speed = 0;
+
+    private bool _Activated;
+
+    void Update()
+    {
+        if (_Activated)
+            transform.position = Vector3.MoveTowards(transform.position, _GotoPosition.position, _Speed);
+    }
+
+    public void StartMoving()
+    {
+        _Activated = true;
+    }
+}
---
+++ Assets/Scripts/Practical/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private Vector3 _RotationSpeed = Vector3.zero;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(_RotationSpeed.x, _RotationSpeed.y, _RotationSpeed.z) * Time.deltaTime);
+    }
+}
