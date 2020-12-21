c5061f324af5f0c220168f00cfae4cbd713ae936
Marcel van Duijn <epicmars264@gmail.com>
2020-12-21 01:37:32 +0100

    Added Interactable.cs + InteractionHandler.cs

---
+++ Assets/Scripts/Useful/Interactable.cs
@@ -0,0 +1,284 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public enum InteractableType {Move, Door, SetLight, SetLightNegative, Lever, Button, Item }
+    public InteractableType _Type;
+
+    private enum AxisOptions {x,y,z}
+    [SerializeField] private AxisOptions _AxisOption = AxisOptions.x;
+
+    [SerializeField] private bool _InvertMouse = false;
+
+    [Header("Type - Light")]
+    [SerializeField] private GameObject _Light = null;
+    [SerializeField] private bool _Light_StartOff = false;
+    [Header("Type - Lever/Door")]
+    [SerializeField] private Transform _LeverRotationPoint = null;
+    [SerializeField] private Vector2 _LeverMinMaxRotation = Vector2.zero;
+    [SerializeField] private float _CompleteDeathZone = 0;
+    [Header("Type - Button")]
+    [SerializeField] private float _ButtonPressDepth = 0;
+    private bool _ButtonPressed;
+    [Header("Type - Item")]
+    [SerializeField] private string _ItemInfo = "";
+    [Header("Speed")]
+    [SerializeField] private float _Speed = 1;
+
+    [Header("OnHigh")]
+    [SerializeField] private UnityEvent _OnHighEvent = null;
+    [Header("OnLow")]
+    [SerializeField] private UnityEvent _OnLowEvent = null;
+    [Header("OnNeutral")]
+    [SerializeField] private UnityEvent _OnNeutral = null;
+    
+
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody _RB;
+    private Vector3 _DefaultLocalPosition;
+    private Vector3 _DefaultPosition;
+    private bool _MovingBack;
+
+    private void Start()
+    {
+        _DefaultLocalPosition = transform.localPosition;
+        _DefaultPosition = transform.position;
+        _RB = GetComponent<Rigidbody>();
+        if(_Type == InteractableType.SetLight || _Type == InteractableType.SetLightNegative)
+        {
+            if (_Light_StartOff)
+                _Light.SetActive(false);
+            else
+                _Light.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (_Type == InteractableType.Button)
+        {
+            UpdateButton();
+        }
+
+        if(_MovingBack)
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, _DefaultLocalPosition, 10 * Time.deltaTime);
+            if (transform.localPosition == _DefaultLocalPosition)
+                _MovingBack = false;
+        }
+    }
+
+    public InteractableType Type()
+    {
+        return _Type;
+    }
+
+    public void GotoPickupPoint(Transform point)
+    {
+        _RB.velocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, point.position, ref velocity, 0.2f);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, point.rotation, 5f);
+    }
+    public void SetVelocity(Vector3 velocity)
+    {
+        _RB.velocity = velocity;
+    }
+    public void TrowObject(Transform transformtrow)
+    {
+        GetComponent<Rigidbody>().AddForce(transformtrow.forward * 5000);
+    }
+    public void OpenDoor()
+    {
+        float mouseY = Input.GetAxis("Mouse Y");
+        float angle = 0;
+        switch (_AxisOption)
+        {
+            case AxisOptions.x:
+                angle = _LeverRotationPoint.localEulerAngles.x;
+                break;
+            case AxisOptions.y:
+                angle = _LeverRotationPoint.localEulerAngles.y;
+                break;
+            case AxisOptions.z:
+                angle = _LeverRotationPoint.localEulerAngles.z;
+                break;
+        }
+        angle = (angle > 180) ? angle - 360 : angle;
+
+        HandleRotation(_LeverRotationPoint, new Vector2(0, mouseY), _LeverMinMaxRotation, 1.2f, angle);
+    }
+    public void MoveLever()
+    {
+        float mouseY = Input.GetAxis("Mouse Y");
+        float angle = 0;
+        switch (_AxisOption)
+        {
+            case AxisOptions.x:
+                angle = _LeverRotationPoint.localEulerAngles.x;
+                break;
+            case AxisOptions.y:
+                angle = _LeverRotationPoint.localEulerAngles.y;
+                break;
+            case AxisOptions.z:
+                angle = _LeverRotationPoint.localEulerAngles.z;
+                break;
+        }
+        angle = (angle > 180) ? angle - 360 : angle;
+
+        HandleRotation(_LeverRotationPoint, new Vector2(0, mouseY), _LeverMinMaxRotation, 1.2f, angle);
+
+        //Check
+        if (angle < _LeverMinMaxRotation.x + _CompleteDeathZone)
+        {
+            _OnLowEvent.Invoke();
+        }
+        if (angle > _LeverMinMaxRotation.y - _CompleteDeathZone)
+        {
+            _OnHighEvent.Invoke();
+        }
+        if (angle > _LeverMinMaxRotation.x + _CompleteDeathZone && angle < _LeverMinMaxRotation.y - _CompleteDeathZone)
+        {
+            _OnNeutral.Invoke();
+        }
+    }
+    public void PressButton(bool option)
+    {
+        _ButtonPressed = true;
+    }
+    public void PressButtonNegative()
+    {
+        _ButtonPressed = !_ButtonPressed;
+    }
+    public void SetLight(bool option)
+    {
+        _Light.SetActive(option);
+    }
+    public void SetLightNegative()
+    {
+        if (_Light.activeSelf)
+            _Light.SetActive(false);
+        else
+            _Light.SetActive(true);
+    }
+    public void ReturnToDefaultPos()
+    {
+        _MovingBack = true;
+    }
+    public string GetItemInfo()
+    {
+        return _ItemInfo;
+    }
+    private void HandleRotation(Transform effectedtransform, Vector2 mousemovement, Vector2 minmaxangle, float speed, float angle)
+    {
+        if (_InvertMouse)
+        {
+            mousemovement.x = mousemovement.x * -2;
+            mousemovement.y = mousemovement.y * -2;
+        }
+
+        switch (_AxisOption)
+        {
+            case AxisOptions.x:
+                effectedtransform.localEulerAngles += new Vector3((mousemovement.x + mousemovement.y) * speed, 0, 0);
+
+                if (angle < minmaxangle.x)
+                    effectedtransform.localEulerAngles = new Vector3(minmaxangle.x + 0.5f, 0, 0);
+                if (angle > minmaxangle.y)
+                    effectedtransform.localEulerAngles = new Vector3(minmaxangle.y - 0.5f, 0, 0);
+                break;
+            case AxisOptions.y:
+                effectedtransform.localEulerAngles += new Vector3(0, (mousemovement.x + mousemovement.y) * speed, 0);
+
+                if (angle < minmaxangle.x)
+                    effectedtransform.localEulerAngles = new Vector3(0, minmaxangle.x + 0.5f, 0);
+                if (angle > minmaxangle.y)
+                    effectedtransform.localEulerAngles = new Vector3(0, minmaxangle.y - 0.5f, 0);
+                break;
+            case AxisOptions.z:
+                effectedtransform.localEulerAngles += new Vector3(0, 0, (mousemovement.x + mousemovement.y) * speed);
+
+                if (angle < minmaxangle.x)
+                    effectedtransform.localEulerAngles = new Vector3(0, 0, minmaxangle.x + 0.5f);
+                if (angle > minmaxangle.y)
+                    effectedtransform.localEulerAngles = new Vector3(0, 0, minmaxangle.y - 0.5f);
+                break;
+        }
+    }
+
+    private void UpdateButton()
+    {
+        switch (_AxisOption)
+        {
+            case AxisOptions.x:
+                if (_ButtonPressed)
+                {
+                    if (transform.localPosition.x > _DefaultLocalPosition.x - _ButtonPressDepth)
+                        transform.localPosition -= new Vector3(_Speed, 0, 0) * Time.deltaTime;
+                    else
+                    { 
+                        transform.localPosition = new Vector3(_DefaultLocalPosition.x - _ButtonPressDepth - 0.001f, transform.localPosition.y, transform.localPosition.z);
+                        _OnLowEvent.Invoke();
+                    }
+                }
+                else
+                {
+                    if (transform.localPosition.x < _DefaultLocalPosition.x + _ButtonPressDepth)
+                        transform.localPosition += new Vector3(_Speed, 0, 0) * Time.deltaTime;
+                    else
+                    { 
+                        transform.localPosition = new Vector3(_DefaultLocalPosition.x + _ButtonPressDepth, transform.localPosition.y, transform.localPosition.z);
+                        _OnHighEvent.Invoke();
+                    }
+
+                }
+                break;
+            case AxisOptions.y:
+                if (_ButtonPressed)
+                {
+                    if (transform.localPosition.y > _DefaultLocalPosition.y - _ButtonPressDepth)
+                        transform.localPosition -= new Vector3(0, _Speed, 0) * Time.deltaTime;
+                    else
+                    { 
+                        transform.localPosition = new Vector3(_DefaultLocalPosition.x, _DefaultLocalPosition.y - _ButtonPressDepth - 0.001f, _DefaultLocalPosition.z);
+                        _OnLowEvent.Invoke();
+                    }
+                }
+                else
+                {
+                    if (transform.localPosition.y < _DefaultLocalPosition.y)
+                        transform.localPosition += new Vector3(0, _Speed, 0) * Time.deltaTime;
+                    else
+                    {
+                        transform.localPosition = _DefaultLocalPosition;
+                        _OnHighEvent.Invoke();
+                    }
+                }
+                break;
+            case AxisOptions.z:
+                if (_ButtonPressed)
+                {
+                    if (transform.localPosition.z > _DefaultLocalPosition.z - _ButtonPressDepth)
+                        transform.localPosition -= new Vector3(0, 0, _Speed) * Time.deltaTime;
+                    else
+                    {
+                        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, _DefaultLocalPosition.z - _ButtonPressDepth - 0.001f);
+                        _OnLowEvent.Invoke();
+                    }
+                }
+                else
+                {
+                    if (transform.localPosition.z < _DefaultLocalPosition.z + _ButtonPressDepth)
+                        transform.localPosition += new Vector3(0, 0, _Speed) * Time.deltaTime;
+                    else
+                    { 
+                        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, _DefaultLocalPosition.z + _ButtonPressDepth);
+                        _OnHighEvent.Invoke();
+                    }
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Useful/InteractionHandler.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InteractionHandler : MonoBehaviour
+{
+    [SerializeField] private Image _Cursor = null;
+    [SerializeField] private LayerMask _LayerMask = 0;
+    [SerializeField] private Transform _Head = null;
+    [Header("Pickup")]
+    [SerializeField] private GameObject _PickupPoint = null;
+    [SerializeField] private Vector2 _PickupMinMaxRange = Vector2.zero;
+    [SerializeField] private float _Range = 0;
+    [Header("Item")]
+    [SerializeField] private Transform _ItemPreviewPoint = null;
+    [SerializeField] private TextMeshProUGUI _ItemInfoText = null;
+
+    private string _ItemInfo;
+
+    private Vector3 _PickupPointPosition;
+    private Vector3 _CalcVelocity;
+    private Vector3 _PrevPosition;
+
+    private GameObject _ActiveObject;
+    private GameObject _CheckObject;
+    private Interactable _Interactable;
+
+    private bool _Interacting;
+    private bool _Previewing;
+
+    private Movement_CC _CCS; //Script that handles rotation
+
+    void Start()
+    {
+        _CCS = GetComponent<Movement_CC>();
+        _PickupPointPosition.z = _PickupMinMaxRange.x;
+    }
+
+    void Update()
+    {
+        if (!_Interacting)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(_Head.position, _Head.TransformDirection(Vector3.forward), out hit, _Range, _LayerMask))
+            {
+                Debug.DrawRay(_Head.position, _Head.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+                _ActiveObject = hit.transform.gameObject;
+
+                _Cursor.color = Color.white;
+            }
+            else
+            {
+                Debug.DrawRay(_Head.position, _Head.TransformDirection(Vector3.forward) * _Range, Color.white);
+                _Cursor.color = Color.red;
+
+                _ActiveObject = null;
+                _CheckObject = null;
+            }
+
+            if (_ActiveObject != _CheckObject)
+            {
+                _Interactable = _ActiveObject.GetComponent<Interactable>();
+                _CheckObject = _ActiveObject;
+            }
+        }
+
+        if(_ActiveObject != null)
+        {
+            if (_Interactable._Type != Interactable.InteractableType.Item)
+            {
+                //OnDown
+                if (Input.GetMouseButtonDown(0))
+                    OnDown();
+
+                if (_Interacting)
+                {
+                    //OnUp
+                    if (Input.GetMouseButtonUp(0))
+                        OnUp();
+
+                    //OnActive
+                    OnActive();
+                }
+            }
+            else
+            {
+                if (!_Previewing)
+                {
+                    //Start Preview
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        _ItemInfo = _Interactable.GetItemInfo();
+                        _CCS.LockRotation(true);
+                        _Previewing = true;
+                    }
+                }
+                else
+                {
+                    _ActiveObject.transform.position = _ItemPreviewPoint.position;
+                    _Interactable.gameObject.transform.eulerAngles += new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+
+                    //Reset Preview
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        _ItemInfo = "";
+                        _CCS.LockRotation(false);
+                        _Interactable.ReturnToDefaultPos();
+                        _Previewing = false;
+                    }
+                }
+            }
+        }
+
+        _ItemInfoText.text = _ItemInfo;
+    }
+
+    void FixedUpdate()
+    {
+        if (_Interacting)
+        {
+            OnActiveFixed();
+            OnActiveFixed();
+        }
+    }
+
+    private void OnUp()
+    {
+        _Interacting = false;
+        switch (_Interactable._Type)
+        {
+            case Interactable.InteractableType.Lever:
+                _CCS.LockRotation(false);
+                break;
+            case Interactable.InteractableType.Door:
+                _CCS.LockRotation(false);
+                break;
+            case Interactable.InteractableType.Move:
+                _Interactable.SetVelocity(_CalcVelocity);
+                break;
+        }
+    }
+    private void OnDown()
+    {
+        _Interacting = true;
+
+        //OnClick
+        switch (_Interactable._Type)
+        {
+            case Interactable.InteractableType.SetLight:
+                _Interactable.SetLight(true);
+                break;
+            case Interactable.InteractableType.SetLightNegative:
+                _Interactable.SetLightNegative();
+                break;
+            case Interactable.InteractableType.Move:
+                _PickupPoint.transform.rotation = _ActiveObject.transform.rotation;
+                _PickupPointPosition.z = Vector3.Distance(_Head.position, _ActiveObject.transform.position);
+                break;
+            case Interactable.InteractableType.Lever:
+                _CCS.LockRotation(true);
+                _PickupPointPosition.z = Vector3.Distance(_Head.position, _ActiveObject.transform.position);
+                break;
+            case Interactable.InteractableType.Door:
+                _CCS.LockRotation(true);
+                break;
+            case Interactable.InteractableType.Button:
+                _Interactable.PressButtonNegative();
+                break;
+        }
+    }
+    private void OnActive()
+    {
+        switch (_Interactable._Type)
+        {
+            case Interactable.InteractableType.Move:
+                if (_PickupPointPosition.z < _PickupMinMaxRange.y && Input.mouseScrollDelta.y > 0)
+                    _PickupPointPosition.z += Input.mouseScrollDelta.y * 0.5f;
+                if (_PickupPointPosition.z > _PickupMinMaxRange.x && Input.mouseScrollDelta.y < 0)
+                    _PickupPointPosition.z += Input.mouseScrollDelta.y * 0.5f;
+
+                if(Input.GetMouseButtonDown(1))
+                {
+                    _Interactable.TrowObject(_Head.transform);
+                    OnUp();
+                }
+                break;
+            case Interactable.InteractableType.Door:
+                _Interactable.OpenDoor();
+                break;
+            case Interactable.InteractableType.Lever:
+                _Interactable.MoveLever();
+                break;
+        }
+
+        if (Vector3.Distance(_Head.transform.position, _ActiveObject.transform.position) > _Range)
+        {
+            _Interacting = false;
+            OnUp();
+        }
+    }
+
+    private void OnActiveFixed()
+    {
+        switch (_Interactable._Type)
+        {
+            case Interactable.InteractableType.Move:
+                _Interactable.GotoPickupPoint(_PickupPoint.transform);
+
+                _PickupPoint.transform.localPosition = _PickupPointPosition;
+
+                _CalcVelocity = (_ActiveObject.transform.position - _PrevPosition) / Time.deltaTime;
+                _PrevPosition = _ActiveObject.transform.position;
+                break;
+        }
+    }
+}
