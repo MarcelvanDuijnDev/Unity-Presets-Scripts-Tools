35230976139aa14094031219bb70bb910e116487
Marcel van Duijn <epicmars264@gmail.com>
2020-12-21 16:01:39 +0100

    Added InteractWithPhysics.cs

---
+++ Assets/Scripts/Practical/InteractWithPhysics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithPhysics : MonoBehaviour
+{
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body != null && !body.isKinematic)
+             body.velocity += hit.controller.velocity;
+    }
+}
