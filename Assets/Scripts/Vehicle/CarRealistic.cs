2e7769b4b32049df97d64b8a2271355532b197e7
Marcel van Duijn <epicmars264@gmail.com>
2020-12-28 00:24:21 +0100

    Updated CarRealistic.cs + Added Car_Realistic.prefab

--- Assets/Scripts/Vehicle/CarRealistic.cs
+++ Assets/Scripts/Vehicle/CarRealistic.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CarRealistic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<AxleInfo> axleInfos = null;
+    [SerializeField] private float maxMotorTorque = 1000;
+    [SerializeField] private float maxSteeringAngle = 50;
+
+    public void FixedUpdate()
     {
-        
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
 }
