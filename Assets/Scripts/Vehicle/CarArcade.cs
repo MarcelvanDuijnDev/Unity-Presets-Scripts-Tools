a9bd1af19dc5bc3d65a82e7eedb1110425b90b8a
Marcel van Duijn <epicmars264@gmail.com>
2021-01-09 16:30:45 +0100

    Update CarArcade.cs

--- Assets/Scripts/Vehicle/CarArcade.cs
+++ Assets/Scripts/Vehicle/CarArcade.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class CarArcade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Settings")]
+    [SerializeField] private float _ForwardAccel = 8;
+    [SerializeField] private float _ReverseAccel = 4;
+    [SerializeField] private float _TurnStrength = 180;
+    [SerializeField] private float _GravityForce = 15;
 
-    // Update is called once per frame
+    [Header("GroundCheck")]
+    [SerializeField] private LayerMask _GroundMask = ~0;
+    [SerializeField] private float _GroundCheckLength = 0.5f;
+
+    [Header("RigidBody")]
+    [SerializeField] private Rigidbody _RB = null;
+
+    private float _SpeedInput;
+    private float _TurnInput;
+    private bool _Grounded;
+
+    void Start() => _RB.transform.parent = null;
+
     void Update()
     {
-        
+        _SpeedInput = 0;
+        if(Input.GetAxis("Vertical") > 0)
+            _SpeedInput = Input.GetAxis("Vertical") * _ForwardAccel * 1000;
+        else if(Input.GetAxis("Vertical") < 0)
+            _SpeedInput = Input.GetAxis("Vertical") * _ReverseAccel * 1000;
+
+        _TurnInput = Input.GetAxis("Horizontal");
+
+        if(_Grounded)
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, _TurnInput * _TurnStrength * Time.deltaTime, 0));
+
+        transform.position = _RB.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        _Grounded = GroundCheck();
+
+        if (_Grounded)
+        {
+            if (Mathf.Abs(_SpeedInput) > 0)
+                _RB.AddForce(transform.forward * _SpeedInput);
+        }
+        else
+            _RB.AddForce(Vector3.up * -_GravityForce * 100);
+    }
+
+    private bool GroundCheck()
+    {
+        _Grounded = false;
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, -transform.up, out hit, _GroundCheckLength, _GroundMask))
+        {
+            _Grounded = true;
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        return _Grounded;
     }
 }
