a1b2b52054ad2643705260c06f9b3e042d057f48
Marcel van Duijn <epicmars264@gmail.com>
2021-07-03 22:07:49 +0200

    Update Tool_FileFinder.cs
    Updates:
    * Added Scene objects information foldout
    * Added Exlude .meta option
    * Fixed errors when deleting gameobject while tool is open

--- Assets/Editor/Tool_FileFinder.cs
+++ Assets/Editor/Tool_FileFinder.cs
@@ -5,35 +5,39 @@ using UnityEngine;
 
 public class Tool_FileFinder : EditorWindow
 {
-    //Tool Mode
+    //Tool State / Scrollpos
     int _ToolState = 0;
     int _ToolStateCheck = 1;
-
-    //FileFinder
-    string _Type = "";
-    string _Search = "";
-    string _SearchCheck = "a";
-    int _Results = 0;
-    int _Total = 0;
     Vector2 _ScrollPos = new Vector2();
 
-    //NameChange
+    //Project
+    string _Project_Type = "";
+    string _Project_Search = "";
+    string _Project_SearchCheck = "a";
+    bool _Project_ExcludeMeta = true;
+    int _Project_Results = 0;
+    int _Project_Total = 0;
 
-    //Scene
-    string _Scene_Search = "";
-    bool _Scene_InsceneInfo = false;
-    GameObject[] _Scene_Objects = new GameObject[0];
-
-    //Results
+    //Project > Results
     string[] _SearchResults = new string[0];
     string[] _SearchResultsChange = new string[0];
 
+    //Scene
+    string _Scene_Search = "";
+    bool _Scene_InsceneInfo = true;
+
+    //Scene > Results
+    bool[] _Scene_Objects_Toggle = new bool[0];
+    GameObject[] _Scene_Objects = new GameObject[0];
+
+    //GetWindow
     [MenuItem("Tools/Tool_FileFinder")]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow(typeof(Tool_FileFinder));
     }
 
+    //Menu/HomePage
     void OnGUI()
     {
         _ToolState = GUILayout.Toolbar(_ToolState, new string[] { "Assets", "Scene" });
@@ -41,7 +45,7 @@ public class Tool_FileFinder : EditorWindow
         if (_ToolState == 0)
         {
             FileFinder_Search();
-            FileFinder_SearchAssets();
+            FileFinder_SearchProject();
         }
         else
         {
@@ -58,71 +62,113 @@ public class Tool_FileFinder : EditorWindow
         } 
     }
 
+    //Project
     void FileFinder_Search()
     {
-        _Search = EditorGUILayout.TextField("Search:", _Search);
-        _Type = EditorGUILayout.TextField("Type:", _Type);
-        GUILayout.Label("(" + _Results + "/" + _Total + ")");
+        _Project_Search = EditorGUILayout.TextField("Search:", _Project_Search);
+        _Project_Type = EditorGUILayout.TextField("Type:", _Project_Type);
+        _Project_ExcludeMeta = EditorGUILayout.Toggle("Exlude Meta:", _Project_ExcludeMeta);
+        GUILayout.Label("(" + _Project_Results + "/" + _Project_Total + ")");
 
-        _Results = 0;
-        _Total = 0;
+        _Project_Results = 0;
+        _Project_Total = 0;
 
-        if (_Search != _SearchCheck)
+        if (_Project_Search != _Project_SearchCheck)
         {
-            _SearchResults = System.IO.Directory.GetFiles("Assets/", "*" + _Type, System.IO.SearchOption.AllDirectories);
+            _SearchResults = System.IO.Directory.GetFiles("Assets/", "*" + _Project_Type, System.IO.SearchOption.AllDirectories);
             _SearchResultsChange = _SearchResults;
-            _SearchCheck = _Search;
+            _Project_SearchCheck = _Project_Search;
         }
     }
-    void FileFinder_SearchAssets()
+    void FileFinder_SearchProject()
     {
         _ScrollPos = EditorGUILayout.BeginScrollView(_ScrollPos);
         for (int i = 0; i < _SearchResults.Length; i++)
         {
-            if (_SearchResults[i].ToLower().Contains(_Search.ToLower()))
+            if (_SearchResults[i].ToLower().Contains(_Project_Search.ToLower()))
             {
-                GUILayout.BeginHorizontal("Box");
-                GUILayout.Label(_SearchResults[i], GUILayout.Width(Screen.width - 80));
-                if (GUILayout.Button("Select", GUILayout.Width(50)))
+                if(_Project_ExcludeMeta)
                 {
-                    Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(_SearchResults[i]);
+                    if (!_SearchResults[i].ToLower().Contains(".meta"))
+                        FileFinder_SearchProject_Results(i);
                 }
-                GUILayout.EndHorizontal();
-                _Results++;
+                else
+                    FileFinder_SearchProject_Results(i);
             }
-            _Total++;
+            _Project_Total++;
         }
         EditorGUILayout.EndScrollView();
     }
+    void FileFinder_SearchProject_Results(int id)
+    {
+        GUILayout.BeginHorizontal("Box");
+        GUILayout.Label(_SearchResults[id], GUILayout.Width(Screen.width - 80));
+        if (GUILayout.Button("Select", GUILayout.Width(50)))
+        {
+            Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(_SearchResults[id]);
+        }
+        GUILayout.EndHorizontal();
+        _Project_Results++;
+    }
 
+    //Scene
     void FileFinder_SceneSearch()
     {
         _Scene_Search = EditorGUILayout.TextField("Search:", _Scene_Search);
-        GUILayout.Label("(" + _Results + "/" + _Total + ")");
+        GUILayout.Label("(" + _Project_Results + "/" + _Project_Total + ")");
 
-        _Results = 0;
-        _Total = 0;
+        if (GUILayout.Button("Refresh"))
+        {
+            _Scene_Objects = new GameObject[0];
+        }
+
+        _Project_Results = 0;
+        _Project_Total = 0;
 
         if (_Scene_Objects.Length == 0)
+        {
             _Scene_Objects = FindObjectsOfType<GameObject>();
+            _Scene_Objects_Toggle = new bool[_Scene_Objects.Length];
+        }
     }
     void FileFinder_Scene()
     {
         _ScrollPos = EditorGUILayout.BeginScrollView(_ScrollPos);
-        for (int i = 0; i < _Scene_Objects.Length; i++)
+        try
         {
-            if (_Scene_Objects[i].name.ToLower().Contains(_Scene_Search.ToLower()))
+            for (int i = 0; i < _Scene_Objects.Length; i++)
             {
-                GUILayout.BeginHorizontal("Box");
-                GUILayout.Label(_Scene_Objects[i].name, GUILayout.Width(Screen.width - 80));
-                if (GUILayout.Button("Select", GUILayout.Width(50)))
+                if (_Scene_Objects[i].name.ToLower().Contains(_Scene_Search.ToLower()))
                 {
-                    Selection.activeObject = _Scene_Objects[i];
+                    GUILayout.BeginHorizontal("Box");
+                    _Scene_Objects_Toggle[i] = EditorGUILayout.Foldout(_Scene_Objects_Toggle[i], "");
+
+                    GUILayout.Label(_Scene_Objects[i].name, GUILayout.Width(Screen.width - 80));
+                    if (GUILayout.Button("Select", GUILayout.Width(50)))
+                    {
+                        Selection.activeObject = _Scene_Objects[i];
+                    }
+
+                    if (_Scene_Objects_Toggle[i])
+                    {
+                        GUILayout.EndHorizontal();
+                        GUILayout.BeginVertical("box");
+                        _Scene_Objects[i].name = EditorGUILayout.TextField("Name:", _Scene_Objects[i].name);
+                        _Scene_Objects[i].transform.position = EditorGUILayout.Vector3Field("Position:", _Scene_Objects[i].transform.position);
+                        _Scene_Objects[i].transform.eulerAngles = EditorGUILayout.Vector3Field("Rotation:", _Scene_Objects[i].transform.eulerAngles);
+                        GUILayout.EndVertical();
+                        GUILayout.BeginHorizontal();
+                    }
+
+                    GUILayout.EndHorizontal();
+                    _Project_Results++;
                 }
-                GUILayout.EndHorizontal();
-                _Results++;
+                _Project_Total++;
             }
-            _Total++;
+        }
+        catch
+        {
+            _Scene_Objects = new GameObject[0];
         }
         EditorGUILayout.EndScrollView();
     }
@@ -133,7 +179,7 @@ public class Tool_FileFinder : EditorWindow
         _ScrollPos = EditorGUILayout.BeginScrollView(_ScrollPos);
         for (int i = 0; i < _SearchResults.Length; i++)
         {
-            if (_SearchResults[i].ToLower().Contains(_Search.ToLower()))
+            if (_SearchResults[i].ToLower().Contains(_Project_Search.ToLower()))
             {
                 GUILayout.BeginHorizontal("Box");
                 _SearchResultsChange[i] = EditorGUILayout.TextField("Object Name: ", _SearchResultsChange[i]);
@@ -152,13 +198,12 @@ public class Tool_FileFinder : EditorWindow
                     Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(_SearchResults[i]);
                 }
                 GUILayout.EndHorizontal();
-                _Results++;
+                _Project_Results++;
             }
-            _Total++;
+            _Project_Total++;
         }
         EditorGUILayout.EndScrollView();
     }
-
 
     //Enable/Disable
     void OnEnable()
@@ -173,18 +218,21 @@ public class Tool_FileFinder : EditorWindow
     //OnSceneGUI
     void OnSceneGUI(SceneView sceneView)
     {
-        Handles.color = new Color(0, 1, 0, 0.3f);
-        if (_Scene_InsceneInfo)
+        try
         {
-            for (int i = 0; i < _Scene_Objects.Length; i++)
+            if (_Scene_InsceneInfo)
             {
-                if (_Scene_Objects[i].name.ToLower().Contains(_Scene_Search.ToLower()))
+                Handles.color = new Color(0, 1, 0, 0.1f);
+                for (int i = 0; i < _Scene_Objects.Length; i++)
                 {
-                    
-                    Handles.SphereHandleCap(1, _Scene_Objects[i].transform.position, Quaternion.identity, 3f, EventType.Repaint);
-                    Handles.Label(_Scene_Objects[i].transform.position, _Scene_Objects[i].name);
+                    if (_Scene_Objects[i].name.ToLower().Contains(_Scene_Search.ToLower()))
+                    {
+                        Handles.SphereHandleCap(1, _Scene_Objects[i].transform.position, Quaternion.identity, 3f, EventType.Repaint);
+                        Handles.Label(_Scene_Objects[i].transform.position, _Scene_Objects[i].name);
+                    }
                 }
             }
         }
+        catch { }
     }
 }
