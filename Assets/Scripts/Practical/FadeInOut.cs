ac3c7f931c271f05169b116f8733857d16bc9163
Marcel van Duijn <epicmars264@gmail.com>
2021-10-22 02:45:39 +0200

    Added FadeInOut.cs
    Added:
    * FadeInOut.cs to practical scripts

---
+++ Assets/Scripts/Practical/FadeInOut.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    private enum Fade { In, Out }
+    [SerializeField] private Fade _FadeOption = Fade.In;
+    [SerializeField] private float _Duration = 0;
+
+    [SerializeField] private Image _Image = null;
+
+    private float _ChangeSpeed;
+    private Color _Color;
+
+    void Start()
+    {
+        if (_Image == null)
+            _Image = GetComponent<Image>();
+
+        if (_FadeOption == Fade.In)
+            _Color = new Color(_Image.color.r, _Image.color.g, _Image.color.b, 0);
+        else
+            _Color = new Color(_Image.color.r, _Image.color.g, _Image.color.b, 1);
+
+        _ChangeSpeed = 1 / _Duration;
+    }
+
+    void Update()
+    {
+        if (_FadeOption == Fade.In && _Color.a < 1)
+        {
+            _Color.a += _ChangeSpeed * Time.deltaTime;
+        }
+        if (_FadeOption == Fade.Out && _Color.a > 0)
+        {
+            _Color.a -= _ChangeSpeed * Time.deltaTime;
+        }
+
+        _Image.color = _Color;
+    }
+}
