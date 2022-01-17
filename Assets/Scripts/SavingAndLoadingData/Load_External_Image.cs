8af6115e61b17177857b58827a1f404d3e3758f8
Marcel van Duijn <epicmars264@gmail.com>
2022-01-17 18:29:37 +0100

    Added Load_External_Image.cs
    * Load Image Files From Outside Unity Project

---
+++ Assets/Scripts/SavingAndLoadingData/Load_External_Image.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_External_Image : MonoBehaviour
+{
+    private string _Path = "";
+
+    [Header("JPG")]
+    [SerializeField] private List<Texture2D> _Images = new List<Texture2D>();
+    private string[] _Files_JPG;
+    private string pathPreFix = @"file://";
+
+    void Start()
+    {
+        _Files_JPG = System.IO.Directory.GetFiles(_Path, "*.jpg");
+        StartCoroutine(LoadImages_BackGround());
+    }
+
+    private IEnumerator LoadImages_BackGround()
+    {
+        //Load JPG
+        foreach (string tstring in _Files_JPG)
+        {
+            string pathTemp = pathPreFix + tstring;
+            WWW www = new WWW(pathTemp);
+            yield return www;
+            Texture2D texTmp = new Texture2D(1024, 1024, TextureFormat.DXT1, false);
+            www.LoadImageIntoTexture(texTmp);
+
+            _Images.Add(texTmp);
+
+            GameObject newobj = null;
+            newobj.transform.eulerAngles = new Vector3(0,0,0);
+        }
+    }
+}
