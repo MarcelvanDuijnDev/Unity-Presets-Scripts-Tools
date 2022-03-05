2f11f58f88ee14fdeaa6cca9be3de9771223c037
Marcel van Duijn <epicmars264@gmail.com>
2022-03-06 00:10:22 +0100

    Update Movement_camera.cs, PosToPos.cs + Tool_QuickStart.cs
    Changes:
    
    * Updated Tool_QuickStart.cs
    * Updated Movement_Camera.cs
    * Updated PosToPos.cs
    * Updated Camera Settings on Multiple Scenes

--- Assets/Scripts/Movement/Movement_Camera.cs
+++ Assets/Scripts/Movement/Movement_Camera.cs
@@ -8,20 +8,19 @@ public class Movement_Camera : MonoBehaviour
     private enum CameraOptionsRot { None, Follow }
 
     [Header("Options")]
+    [SerializeField] private Transform _Target = null;
     [SerializeField] private CameraOptionsPos _CameraOptionPos = CameraOptionsPos.Follow;
     [SerializeField] private CameraOptionsRot _CameraOptionRot = CameraOptionsRot.Follow;
 
-    [Header("Settings - Position")]
+    [Header("Offset")]
     [SerializeField] private Vector3 _OffsetPosition = new Vector3(0,12,-4);
-
-    [Header("Settings - Rotation")]
     [SerializeField] private Vector3 _OffsetRotation = Vector3.zero;
 
     [Header("Settings")]
-    [SerializeField] private float _Speed = 1000;
+    [SerializeField] private float _Speed = 3;
+    [SerializeField] private bool _LerpPosition = true;
 
-    [Header("Other")]
-    [SerializeField] private Transform _Target = null;
+    [Header("Contraints")]
     [SerializeField] private bool _LockAxis_X = false;
     [SerializeField] private bool _LockAxis_Y = false;
     [SerializeField] private bool _LockAxis_Z = false;
@@ -32,16 +31,12 @@ public class Movement_Camera : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.J))
-        {
-            Effect_ScreenShake(3, 0.5f);
-        }
-
         //Update Target Location
         float x_axis = _Target.transform.position.x + _OffsetPosition.x;
         float y_axis = _Target.transform.position.y + _OffsetPosition.y;
         float z_axis = _Target.transform.position.z + _OffsetPosition.z;
 
+        //Lock Axis
         if (_LockAxis_X)
             x_axis = _OffsetPosition.x;
         if (_LockAxis_Y)
@@ -51,18 +46,21 @@ public class Movement_Camera : MonoBehaviour
 
         _TargetPosition = new Vector3(x_axis, y_axis, z_axis);
 
-        // Movement
+        //Movement
         switch (_CameraOptionPos)
         {
             case CameraOptionsPos.Follow:
-                transform.position = Vector3.MoveTowards(transform.position, _TargetPosition, _Speed * Time.deltaTime);
+                if (_LerpPosition)
+                    transform.position = Vector3.Lerp(transform.position, _TargetPosition, _Speed * Time.deltaTime);
+                else
+                    transform.position = Vector3.MoveTowards(transform.position, _TargetPosition, _Speed * Time.deltaTime);
                 break;
         }
 
         //ScreenShake
         if(_ScreenShakeDuration > 0)
         {
-            transform.localPosition = new Vector3(_TargetPosition.x + Random.insideUnitSphere.x * _ScreenShakeIntensity, _TargetPosition.y + Random.insideUnitSphere.y * _ScreenShakeIntensity, _TargetPosition.z);
+            transform.localPosition = new Vector3(transform.position.x + Random.insideUnitSphere.x * _ScreenShakeIntensity, transform.position.y + Random.insideUnitSphere.y * _ScreenShakeIntensity, transform.position.z);
             _ScreenShakeDuration -= 1 * Time.deltaTime;
         }
         else
--- Assets/Scripts/Practical/PosToPos.cs
+++ Assets/Scripts/Practical/PosToPos.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PosToPos : MonoBehaviour
 {
+    [Header("Target Position")]
     [SerializeField] private Transform _GotoPosition = null;
-    [SerializeField] private float _Speed = 0;
+
+    [Header("Settings")]
+    [SerializeField] private float _Speed = 1;
+    [SerializeField] private bool _Lerp = false;
+    [SerializeField] private bool _OnStart = false;
 
     private bool _Activated;
+
+    private void Start()
+    {
+        if (_OnStart)
+            StartMoving();
+    }
 
     void Update()
     {
         if (_Activated)
-            transform.position = Vector3.MoveTowards(transform.position, _GotoPosition.position, _Speed);
+            if (_Lerp)
+                transform.position = Vector3.Lerp(transform.position, _GotoPosition.position, _Speed * Time.deltaTime);
+            else
+                transform.position = Vector3.MoveTowards(transform.position, _GotoPosition.position, _Speed * Time.deltaTime);
     }
 
     public void StartMoving()
