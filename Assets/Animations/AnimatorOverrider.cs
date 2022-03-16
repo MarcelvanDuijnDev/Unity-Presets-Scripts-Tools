68b42ba2c179e55ac193934535c840c8deed27cb
Marcel van Duijn <epicmars264@gmail.com>
2022-03-16 15:37:35 +0100

    Added AnimatorOverrider.cs + Example animations / testscene
    Changes:
    
    * Added Animations Folder
    * Added a / b / c.overrideController.meta
    * Added AnimatorExample.controller
    * Added AnimatorOverrider.cs
    * Added Xbot.fbx > +3 Animations
    * Added Animation.unity
    
    * Updated ExampleScene_Interaction.unity

---
+++ Assets/Animations/AnimatorOverrider.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorOverrider : MonoBehaviour
+{
+    [SerializeField] private AnimatorOverrideController[] _OverrideControllers;
+    private Animator _Animator;
+
+    private void Awake()
+    {
+        _Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Set(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Set(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Set(2);
+        }
+    }
+
+    public void SetAnimations(AnimatorOverrideController overridecontroller)
+    {
+        _Animator.runtimeAnimatorController = overridecontroller;
+    }
+
+    public void Set(int value)
+    {
+        SetAnimations(_OverrideControllers[value]);
+    }
+}
