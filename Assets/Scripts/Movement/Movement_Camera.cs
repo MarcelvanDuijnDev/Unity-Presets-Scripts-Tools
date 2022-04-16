a719b14784fa02838bfa1f7404afc33e2c866571
Marcel van Duijn <epicmars264@gmail.com>
2022-04-16 15:38:54 +0200

    Update Movement_Camera.cs + Added Movement_Camera_CutScenes.cs

--- Assets/Scripts/Movement/Movement_Camera.cs
+++ Assets/Scripts/Movement/Movement_Camera.cs
@@ -13,7 +13,7 @@ public class Movement_Camera : MonoBehaviour
     [SerializeField] private CameraOptionsRot _CameraOptionRot = CameraOptionsRot.Follow;
 
     [Header("Offset")]
-    [SerializeField] private Vector3 _OffsetPosition = new Vector3(0,12,-4);
+    [SerializeField] private Vector3 _OffsetPosition = new Vector3(0, 12, -4);
     [SerializeField] private Vector3 _OffsetRotation = Vector3.zero;
 
     [Header("Settings")]
@@ -29,6 +29,20 @@ public class Movement_Camera : MonoBehaviour
     private float _ScreenShakeDuration;
     private float _ScreenShakeIntensity;
 
+    //CutScenes
+    private bool _CutScene;
+    private GameObject _CutSceneTarget;
+    private Vector3 _CutScenePosition;
+    private Vector3 _CutSceneRotation;
+    private bool _CutSceneFollow;
+
+    public static Movement_Camera CAM;
+
+    private void Awake()
+    {
+        CAM = this;
+    }
+
     void Update()
     {
         //Update Target Location
@@ -36,43 +50,68 @@ public class Movement_Camera : MonoBehaviour
         float y_axis = _Target.transform.position.y + _OffsetPosition.y;
         float z_axis = _Target.transform.position.z + _OffsetPosition.z;
 
-        //Lock Axis
-        if (_LockAxis_X)
-            x_axis = _OffsetPosition.x;
-        if (_LockAxis_Y)
-            y_axis = _OffsetPosition.y;
-        if (_LockAxis_Z)
-            z_axis = _OffsetPosition.z;
-
-        _TargetPosition = new Vector3(x_axis, y_axis, z_axis);
-
-        //Movement
-        switch (_CameraOptionPos)
+        if (!_CutScene)
         {
-            case CameraOptionsPos.Follow:
-                if (_LerpPosition)
-                    transform.position = Vector3.Lerp(transform.position, _TargetPosition, _Speed * Time.deltaTime);
-                else
-                    transform.position = Vector3.MoveTowards(transform.position, _TargetPosition, _Speed * Time.deltaTime);
-                break;
-        }
+            //Lock Axis
+            if (_LockAxis_X)
+                x_axis = _OffsetPosition.x;
+            if (_LockAxis_Y)
+                y_axis = _OffsetPosition.y;
+            if (_LockAxis_Z)
+                z_axis = _OffsetPosition.z;
 
-        //ScreenShake
-        if(_ScreenShakeDuration > 0)
-        {
-            transform.localPosition = new Vector3(transform.position.x + Random.insideUnitSphere.x * _ScreenShakeIntensity, transform.position.y + Random.insideUnitSphere.y * _ScreenShakeIntensity, transform.position.z);
-            _ScreenShakeDuration -= 1 * Time.deltaTime;
+            _TargetPosition = new Vector3(x_axis, y_axis, z_axis);
+
+            //Movement
+            switch (_CameraOptionPos)
+            {
+                case CameraOptionsPos.Follow:
+                    if (_LerpPosition)
+                        transform.position = Vector3.Lerp(transform.position, _TargetPosition, _Speed * Time.deltaTime);
+                    else
+                        transform.position = Vector3.MoveTowards(transform.position, _TargetPosition, _Speed * Time.deltaTime);
+                    break;
+            }
+
+            //ScreenShake
+            if (_ScreenShakeDuration > 0)
+            {
+                transform.localPosition = new Vector3(transform.position.x + Random.insideUnitSphere.x * _ScreenShakeIntensity, transform.position.y + Random.insideUnitSphere.y * _ScreenShakeIntensity, transform.position.z);
+                _ScreenShakeDuration -= 1 * Time.deltaTime;
+            }
+            else
+            {
+                // Rotation
+                switch (_CameraOptionRot)
+                {
+                    case CameraOptionsRot.Follow:
+                        Vector3 rpos = _Target.position - transform.position;
+                        Quaternion lookrotation = Quaternion.LookRotation(rpos, Vector3.up);
+                        transform.eulerAngles = new Vector3(lookrotation.eulerAngles.x + _OffsetRotation.x, lookrotation.eulerAngles.y + _OffsetRotation.y, lookrotation.eulerAngles.z + _OffsetRotation.z);
+                        break;
+                }
+            }
         }
         else
         {
-            // Rotation
-            switch (_CameraOptionRot)
+            //Position
+            _TargetPosition = _CutScenePosition;
+
+            if (_LerpPosition)
+                transform.position = Vector3.Lerp(transform.position, _TargetPosition, _Speed * Time.deltaTime);
+            else
+                transform.position = Vector3.MoveTowards(transform.position, _TargetPosition, _Speed * Time.deltaTime);
+
+            //Target
+            if (_CutSceneFollow)
             {
-                case CameraOptionsRot.Follow:
-                    Vector3 rpos = _Target.position - transform.position;
-                    Quaternion lookrotation = Quaternion.LookRotation(rpos, Vector3.up);
-                    transform.eulerAngles = new Vector3(lookrotation.eulerAngles.x + _OffsetRotation.x, lookrotation.eulerAngles.y + _OffsetRotation.y, lookrotation.eulerAngles.z + _OffsetRotation.z);
-                    break;
+                Vector3 rpos = _CutSceneTarget.transform.position - transform.position;
+                Quaternion lookrotation = Quaternion.LookRotation(rpos, Vector3.up);
+                transform.eulerAngles = new Vector3(lookrotation.eulerAngles.x + _CutSceneRotation.x, lookrotation.eulerAngles.y + _CutSceneRotation.y, lookrotation.eulerAngles.z + _CutSceneRotation.z);
+            }
+            else
+            {
+                transform.eulerAngles = _CutSceneRotation;
             }
         }
     }
@@ -84,13 +123,34 @@ public class Movement_Camera : MonoBehaviour
         _ScreenShakeIntensity = intesity;
     }
 
-    //Set
-    public void Set_CameraTarget(GameObject targetobj)
+    //GetSet
+    public Transform CameraTarget
     {
-        _Target = targetobj.transform;
+        get { return _Target; }
+        set { _Target = value; }
     }
-    public void Set_OffSet(Vector3 offset)
+    public Vector3 Camera_OffSetPosition
     {
-        _OffsetPosition = offset;
+        get { return _OffsetPosition; }
+        set { _OffsetPosition = value; }
+    }
+    public Vector3 Camera_OffSetRotation
+    {
+        get { return _OffsetRotation; }
+        set { _OffsetRotation = value; }
+    }
+
+    //CutScene
+    public void CutScene(bool startcutscene)
+    {
+        _CutScene = false;
+    }
+    public void CutScene(bool startcutscene, Vector3 cutscenepos, GameObject targetobj, Vector3 cutscenerot, bool follow)
+    {
+        _CutScene = startcutscene;
+        _CutScenePosition = cutscenepos;
+        _CutSceneTarget = targetobj;
+        _CutSceneRotation = cutscenerot;
+        _CutSceneFollow = follow;
     }
 }
---
+++ Assets/Scripts/Movement/Movement_Camera_CutScenes.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Camera_CutScenes : MonoBehaviour
+{
+    [SerializeField] private List<Movement_Camera_CutScene> _CutScenes = new List<Movement_Camera_CutScene>();
+    [SerializeField] private LayerMask _TargetLayer;
+
+    private int _Current_CutScene;
+
+    [Header("Debug Gizmos")]
+    [SerializeField] private int _Gizmos_Current_CutScene;
+
+    void Update()
+    {
+        //If OnTrigger
+        for (int i = 0; i < _CutScenes.Count; i++)
+        {
+            if (_CutScenes[i].OnTrigger)
+            {
+                if (_CutScenes[i].CurrrentScene < _CutScenes[i].CutScene_Scenes.Count)
+                {
+                    int detectplayer = Physics.BoxCastAll(_CutScenes[i].CutSceneTriggerPos.position, _CutScenes[i].Size, transform.forward, Quaternion.identity, 1, _TargetLayer).Length;
+
+                    if (detectplayer > 0)
+                        Movement_Camera.CAM.CutScene(true, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutScenePosition, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutSceneTarget, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutSceneRotation, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].FollowTarget);
+                    else
+                        Movement_Camera.CAM.CutScene(false);
+                }
+            }
+        }
+    }
+
+    public void NextScene()
+    {
+        _CutScenes[_Current_CutScene].CurrrentScene++;
+        if (_CutScenes[_Current_CutScene].CurrrentScene < _CutScenes[_Current_CutScene].CutScene_Scenes.Count)
+            Movement_Camera.CAM.CutScene(true, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutScenePosition, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutSceneTarget, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutSceneRotation, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].FollowTarget);
+        else
+            Movement_Camera.CAM.CutScene(false);
+    }
+    public void Set_CutScene(int cutsceneid)
+    {
+        _Current_CutScene = cutsceneid;
+    }
+    public void Set_Scene(int sceneid)
+    {
+        _CutScenes[_Current_CutScene].CurrrentScene = sceneid;
+    }
+    public void Set_Scene(int sceneid, bool startscene)
+    {
+        _CutScenes[_Current_CutScene].CurrrentScene = sceneid;
+        if (startscene == true)
+        {
+            Start_CutScene();
+        }
+    }
+    public void Start_CutScene()
+    {
+        Movement_Camera.CAM.CutScene(true, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutScenePosition, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutSceneTarget, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].CutSceneRotation, _CutScenes[_Current_CutScene].CutScene_Scenes[_CutScenes[_Current_CutScene].CurrrentScene].FollowTarget);
+    }
+    public void Stop_CutScene()
+    {
+        Movement_Camera.CAM.CutScene(false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_Gizmos_Current_CutScene < _CutScenes.Count)
+        {
+            //Trigger Position
+            Gizmos.color = new Vector4(0, 1, 0, 0.2f);
+            if (_CutScenes[_Gizmos_Current_CutScene].OnTrigger)
+                Gizmos.DrawCube(_CutScenes[_Gizmos_Current_CutScene].CutSceneTriggerPos.position, _CutScenes[_Gizmos_Current_CutScene].Size);
+
+            //Camera Positions
+            for (int i = 0; i < _CutScenes[_Gizmos_Current_CutScene].CutScene_Scenes.Count; i++)
+            {
+                //OnFollow / Locked
+                if (_CutScenes[_Gizmos_Current_CutScene].CutScene_Scenes[i].FollowTarget)
+                    Gizmos.color = new Vector4(0, 1, 0, 0.2f);
+                else
+                    Gizmos.color = new Vector4(1, 0, 0, 0.2f);
+
+                //DrawCamera
+                Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
+                Gizmos.matrix = Matrix4x4.TRS(_CutScenes[_Gizmos_Current_CutScene].CutScene_Scenes[i].CutScenePosition, Quaternion.Euler(_CutScenes[_Gizmos_Current_CutScene].CutScene_Scenes[i].CutSceneRotation), Vector4.one);
+                Gizmos.DrawFrustum(Vector4.zero, 60, 10, 0.5f, 1);
+                Gizmos.matrix = oldGizmosMatrix;
+                Gizmos.DrawSphere(_CutScenes[_Gizmos_Current_CutScene].CutScene_Scenes[i].CutScenePosition, 0.5f);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class Movement_Camera_CutScene
+{
+    public string Info;
+
+    public List<Movement_Camera_CutSceneLocation> CutScene_Scenes;
+    public int CurrrentScene;
+
+    [Header("Trigger Area")]
+    public bool OnTrigger;
+    public Transform CutSceneTriggerPos;
+    public Vector3 Size;
+}
+
+[System.Serializable]
+public class Movement_Camera_CutSceneLocation
+{
+    public string Info;
+
+    [Header("CutScene Settings")]
+    public Vector3 CutScenePosition;
+    public Vector3 CutSceneRotation;
+
+    [Header("Follow Target")]
+    public bool FollowTarget;
+    public GameObject CutSceneTarget;
+}
